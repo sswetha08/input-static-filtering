7c5eca03040142a47ded53d584079e3469f2fd73
swethasairam <sswetha08@gmail.com>
2023-04-29 13:31:41 -0400

    capture assign nodes and key points

--- src/input-analyzer/InputAnalyzer.cpp
+++ src/input-analyzer/InputAnalyzer.cpp
@@ -1,22 +1,30 @@
-// Declares clang::SyntaxOnlyAction.
 #include "clang/Frontend/FrontendActions.h"
+#include "clang/Frontend/CompilerInstance.h"
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/Tooling/Tooling.h"
-// Declares llvm::cl::extrahelp.
+
 #include "llvm/Support/CommandLine.h"
 
 #include "clang/ASTMatchers/ASTMatchers.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/AST/Comment.h"
+#include "clang/AST/CommentVisitor.h"
+#include "clang/AST/RecursiveASTVisitor.h"
 
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace clang;
 using namespace clang::ast_matchers;
 using namespace clang::tooling;
+using namespace clang::comments;
 using namespace llvm;
 
 std::vector<std::string> inputVars;
+
+// Create a map of var names to represent flowing value relatioship
+std::map<std::string, std::string> valFlow;
 
 StatementMatcher getcMatcher = callExpr(
   callee(functionDecl(hasName("getc"))),
@@ -28,6 +36,97 @@ StatementMatcher getcMatcher = callExpr(
 StatementMatcher scanfMatcher =
   callExpr(callee(functionDecl(hasName("scanf")))).bind("scanfCall");
 
+// Define a matcher for label statements
+StatementMatcher labelMatcher = labelStmt().bind("label");
+
+StatementMatcher assignMatcher = binaryOperator(
+    hasOperatorName("="),
+    hasLHS(declRefExpr().bind("lhs")),
+    hasRHS(expr(hasDescendant(declRefExpr())).bind("rhs"))
+).bind("assign");
+
+// Define the callback function to handle matched statements
+class AssignHandler : public MatchFinder::MatchCallback {
+public:
+  virtual void run(const MatchFinder::MatchResult &Result) {
+    // Get the matched AST nodes
+    const auto *LhsDeclRef = Result.Nodes.getNodeAs<DeclRefExpr>("lhs");
+    const auto *RhsExpr = Result.Nodes.getNodeAs<Expr>("rhs");
+    if (LhsDeclRef && RhsExpr) {
+      llvm::errs() << "Found assignment statement with LHS: " << LhsDeclRef->getNameInfo().getAsString() << "\n";
+      llvm::errs() << "RHS DeclRefExprs: \n";
+      const auto *RhsSubExpr = RhsExpr->IgnoreImplicit()->IgnoreParens();
+      if (const auto *RhsDeclRef = dyn_cast<DeclRefExpr>(RhsSubExpr)) {
+        llvm::errs() << "\t" << RhsDeclRef->getNameInfo().getAsString() << "\n";
+      } 
+      else if (const auto *binaryRHS = dyn_cast<BinaryOperator>(RhsSubExpr))
+      {
+          const auto* lhsRHS = dyn_cast<Expr>(binaryRHS->getLHS());
+          const auto* lhsRHSI = lhsRHS->IgnoreImplicit()->IgnoreParens();
+          const auto* rhsRHS = dyn_cast<Expr>(binaryRHS->getRHS());
+          const auto* rhsRHSI = rhsRHS->IgnoreImplicit()->IgnoreParens();
+
+          const auto* l = dyn_cast<DeclRefExpr>(lhsRHSI);
+          const auto* r = dyn_cast<DeclRefExpr>(rhsRHSI);
+
+          if (l || r) {
+            if (l)
+              llvm::errs() << "RHS LHS: " << l->getNameInfo().getName().getAsString() << "\n";
+            if (r)
+              llvm::errs() << "RHS RHS: " << r->getNameInfo().getName().getAsString() << "\n";
+          }        
+      }
+    }
+  }
+};
+
+class LabelPrinter : public MatchFinder::MatchCallback {
+public:
+  virtual void run(const MatchFinder::MatchResult &Result) {
+    // Get the matched statement
+    const LabelStmt *Label = Result.Nodes.getNodeAs<LabelStmt>("label");
+    if (Label) {
+      StringRef LabelName = Label->getName();
+        if (LabelName.contains("key_point")) {
+          // Print the label name and line number
+          llvm::outs() << LabelName
+                      << " at line " << Result.Context->getSourceManager().getSpellingLineNumber(Label->getBeginLoc())
+                      << "\n";
+      }
+    }
+  }
+};
+
+
+class CommentFinder : public clang::ASTConsumer {
+public:
+  CommentFinder(clang::ASTContext  &ASTContext) {
+  }
+
+  void HandleTranslationUnit(clang::ASTContext &context) final {
+    auto comments = context.Comments.getCommentsInFile(
+      context.getSourceManager().getMainFileID());
+    llvm::outs() << context.Comments.empty() << "\n";
+    if(!context.Comments.empty())
+    {
+      for (auto it = comments->begin(); it != comments->end(); it++) {
+        clang::RawComment* comment = it->second;
+        std::string source = comment->getFormattedText(context.getSourceManager(),
+            context.getDiagnostics());
+        llvm::outs() << "source:" << source << "\n";
+      }
+    }
+  }
+};
+
+class MyFrontendAction : public clang::ASTFrontendAction {
+public:
+  std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(
+    clang::CompilerInstance& CI, clang::StringRef) final {
+    return std::unique_ptr<clang::ASTConsumer>(
+        new CommentFinder(CI.getASTContext()));
+  }
+};
 
 class ScanfExtractor : public MatchFinder::MatchCallback {
 public :
@@ -101,9 +200,13 @@ int main(int argc, const char **argv) {
   GetcExtractor getcHandler;
   ScanfExtractor scanfHandler;
   MatchFinder Finder;
+  LabelPrinter labelHandler;
+  AssignHandler assignHandler;
 
   Finder.addMatcher(getcMatcher, &getcHandler);
   Finder.addMatcher(scanfMatcher, &scanfHandler);
+  Finder.addMatcher(labelMatcher, &labelHandler);
+  Finder.addMatcher(assignMatcher, &assignHandler);
 
   Tool.run(newFrontendActionFactory(&Finder).get());
 
@@ -114,4 +217,6 @@ int main(int argc, const char **argv) {
      llvm::outs()<< var <<" ";
   }
   llvm::outs() <<"\n";
+
+  return 0;
 }
